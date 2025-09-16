3b28c910a41cc6d3497292282ccc4c9f40b7b98a
kayetavi <avijitkayet97@gmail.com>
2025-09-16 22:37:37 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,15 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Updated Firebase Config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAEWP0LKsZ_SO4FZ9KP7THVILztzFxcquQ",
+  authDomain: "api-damage-438cb.firebaseapp.com",
+  projectId: "api-damage-438cb",
+  storageBucket: "api-damage-438cb.appspot.com",
+  messagingSenderId: "255366494533",
+  appId: "1:255366494533:web:ed628a7e255b472b55262c"
 };
 
+// Initialize Firebase App
 const app = initializeApp(firebaseConfig);
+
+// Export Auth & Firestore
 export const auth = getAuth(app);
 export const db = getFirestore(app);
